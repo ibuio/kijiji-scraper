ad992ae261304178b804b9f16980a3c25f979d40
jon <j.klang@chronometriq.ca>
2016-09-12 21:52:23 -0400

    testing

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ class Ad {
         //ad.price = $jquerySelector.find('.price').text().trim();
 
         // keep add only if add date is from 1 hour or less (fr + en)
-        if(adDate.indexOf("< 1 hour") !== -1 || adDate.indexOf("1 heure") !== -1 || adDate.indexOf("minutes") !== -1)
+        if(adDate.indexOf("< 2 hours") !== -1 || adDate.indexOf("2 heure") !== -1 || adDate.indexOf("< 1 hour") !== -1 || adDate.indexOf("1 heure") !== -1 || adDate.indexOf("minutes") !== -1)
             return ad;
     }
 
