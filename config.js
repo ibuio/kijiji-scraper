f71d04aef7fb4a8ad878a28382865b7df054f0aa
jon <j.klang@chronometriq.ca>
2016-09-22 22:15:44 -0400

    configs for classic AND regular ads

--- config.js
+++ config.js
@@ -9,7 +9,31 @@ var config = {
     // 'http://www.kijiji.ca/b-cars-trucks/alberta/ferrari__lamborghini__porsche-1968__1995/c174l9003a54a68?ad=offering',
     // 'http://www.kijiji.ca/b-cars-trucks/british-columbia/ferrari__lamborghini__porsche-1968__1995/c174l9007a54a68?ad=offering'
     urls: [
-        'http://www.kijiji.ca/b-classic-cars/canada/bmw__ferrari__lamborghini__porsche-308__328__348__355__356__360__430__456__535+gran+turismo__550__575__911__912__914__928__930__944__968__gallardo__m3__mondial__murcielago__testarossa-1968__1991/c122l0a142a1000142a95?ad=offering&siteLocale=fr_CA'
+        'http://www.kijiji.ca/b-autos-camions/quebec/autre+type+de+carrosserie__berline__bicorps__cabriolet__coupe__familiale-porsche-356__912__914__924__928__930__944__968__boxster__carrera-1968__1998/c174l9001a138a54a1000054a68?ad=offering',
+        'http://www.kijiji.ca/b-cars-trucks/alberta/convertible__coupe__hatchback__other+body+type__sedan__wagon-porsche-356__912__914__924__928__930__944__968__boxster__carrera-1968__1998/c174l9003a138a54a1000054a68',
+        'http://www.kijiji.ca/b-cars-trucks/british-columbia/convertible__coupe__hatchback__other+body+type__sedan__wagon-porsche-356__912__914__924__928__930__944__968__boxster__carrera-1968__1998/c174l9007a138a54a1000054a68',
+        'http://www.kijiji.ca/b-cars-trucks/manitoba/convertible__coupe__hatchback__other+body+type__sedan__wagon-porsche-356__912__914__924__928__930__944__968__boxster__carrera-1968__1998/c174l9006a138a54a1000054a68',
+        'http://www.kijiji.ca/b-cars-trucks/new-brunswick/convertible__coupe__hatchback__other+body+type__sedan__wagon-porsche-356__912__914__924__928__930__944__968__boxster__carrera-1968__1998/c174l9005a138a54a1000054a68',
+        'http://www.kijiji.ca/b-cars-trucks/nova-scotia/convertible__coupe__hatchback__other+body+type__sedan__wagon-porsche-356__912__914__924__928__930__944__968__boxster__carrera-1968__1998/c174l9002a138a54a1000054a68',
+        'http://www.kijiji.ca/b-cars-trucks/ontario/convertible__coupe__hatchback__other+body+type__sedan__wagon-porsche-356__912__914__924__928__930__944__968__boxster__carrera-1968__1998/c174l9004a138a54a1000054a68',
+        'http://www.kijiji.ca/b-autos-camions/quebec/autre+type+de+carrosserie__berline__bicorps__cabriolet__coupe__familiale-porsche-911/c174l9001a138a54a1000054',
+        'http://www.kijiji.ca/b-cars-trucks/alberta/convertible__coupe__hatchback__other+body+type__sedan__wagon-porsche-911/c174l9003a138a54a1000054',
+        'http://www.kijiji.ca/b-cars-trucks/british-columbia/convertible__coupe__hatchback__other+body+type__sedan__wagon-porsche-911/c174l9007a138a54a1000054',
+        'http://www.kijiji.ca/b-cars-trucks/manitoba/convertible__coupe__hatchback__other+body+type__sedan__wagon-porsche-911/c174l9006a138a54a1000054',
+        'http://www.kijiji.ca/b-cars-trucks/new-brunswick/convertible__coupe__hatchback__other+body+type__sedan__wagon-porsche-911/c174l9005a138a54a1000054',
+        'http://www.kijiji.ca/b-cars-trucks/nova-scotia/convertible__coupe__hatchback__other+body+type__sedan__wagon-porsche-911/c174l9002a138a54a1000054',
+        'http://www.kijiji.ca/b-cars-trucks/ontario/convertible__coupe__hatchback__other+body+type__sedan__wagon-porsche-911/c174l9004a138a54a1000054',
+        'http://www.kijiji.ca/b-autos-camions/quebec/ferrari__lamborghini-1968__1998/c174l9001a54a68?ad=offering',
+        'http://www.kijiji.ca/b-cars-trucks/alberta/ferrari__lamborghini-1968__1998/c174l9003a54a68',
+        'http://www.kijiji.ca/b-cars-trucks/british-columbia/ferrari__lamborghini-1968__1998/c174l9007a54a68',
+        'http://www.kijiji.ca/b-cars-trucks/manitoba/ferrari__lamborghini-1968__1998/c174l9006a54a68',
+        'http://www.kijiji.ca/b-cars-trucks/new-brunswick/ferrari__lamborghini-1968__1998/c174l9005a54a68',
+        'http://www.kijiji.ca/b-cars-trucks/nova-scotia/ferrari__lamborghini-1968__1998/c174l9002a54a68',
+        'http://www.kijiji.ca/b-cars-trucks/ontario/ferrari__lamborghini-1968__1998/c174l9004a54a68',
+        'http://www.kijiji.ca/b-classic-cars/canada/bmw-m3-1982__1991/c122l0a142a1000142a95',
+        'http://www.kijiji.ca/b-classic-cars/canada/porsche-356__912__914__924__928__930__944__968__boxster__carrera__cayenne__cayman__other+model__panamera__panamera_hybrid-1968__1998/c122l0a142a1000142a95?ad=offering',
+        'http://www.kijiji.ca/b-classic-cars/canada/porsche-911/c122l0a142a1000142?ad=offering',
+        'http://www.kijiji.ca/b-classic-cars/canada/ferrari__lamborghini/c122l0a142?ad=offering'
     ],
 
     email: {
